d9fcb19a7e575a13664df3d7e6e4fbda709b9373
eleanor <eleanorgklein@gmail.com>
2025-04-07 18:48:54 -0500

    stated newGame

--- Nim.cpp
+++ Nim.cpp
@@ -2,19 +2,25 @@ using namespace std;
 
 #include <iostream>
 
-void removeRow(int rowNum); 
-void endTurn(); 
-void newGame(); 
-
 int row1Sticks;
+int row2Sticks;
 int row3Sticks;
-int row5Sticks;
-int row7Sticks;
+int row4Sticks;
 int currentRow;
 bool takenStick;
 bool currentTurn;
 bool gameWon;
 
+void removeRow(int rowNum); 
+void endTurn(); 
+
+void newGame()
+{
+    row1Sticks = 1;
+    row2Sticks = 3;
+    row3Sticks = 5;
+    row4Sticks = 7; 
+} 
 
 int main()
 {
