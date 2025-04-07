a5bf22021938effda97c5ca4992a19c16d9234bb
eleanor <eleanorgklein@gmail.com>
2025-04-07 18:37:28 -0500

    created file and did some basic structure

---
+++ Nim.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+
+#include <iostream>
+
+void removeRow(int rowNum); 
+void endTurn(); 
+void newGame(); 
+
+int row1Sticks;
+int row3Sticks;
+int row5Sticks;
+int row7Sticks;
+int currentRow;
+bool takenStick;
+bool currentTurn;
+bool gameWon;
+
+
+int main()
+{
+
+}
